f58a7ff13095fe3ed26edfd358c4cb87a0c8411f
navas005 <navasmhd005@gmail.com>
2025-11-27 09:41:13 +0530

    completed

--- lib/bmi.dart
+++ lib/bmi.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 
-class Bmi extends StatelessWidget {
+class Bmi extends StatefulWidget {
   const Bmi({super.key});
 
+  @override
+  State<Bmi> createState() => _BmiState();
+}
+
+class _BmiState extends State<Bmi> {
+  TextEditingController heightcontroller=TextEditingController();
+  TextEditingController weightcontroller=TextEditingController();
+  String result="";
+  void calculate_bmi(){
+    double height=double.parse(heightcontroller.text)/100;
+    double weight=double.parse(weightcontroller.text);
+    double bmi=weight/(height*height);
+    setState(() {result="your bmi is ${bmi.toStringAsFixed(2)}";
+      
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,7 +51,7 @@ class Bmi extends StatelessWidget {
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        TextField(
+                        TextField(controller: weightcontroller,
                           decoration: InputDecoration(
                             prefixIcon: Icon(Icons.monitor_weight),
                             hintText: "Enter weight in KG",
@@ -52,7 +68,7 @@ class Bmi extends StatelessWidget {
                             ),
                           ),
                         ),
-                        TextField(
+                        TextField(controller: heightcontroller,
                           decoration: InputDecoration(
                             prefixIcon: Icon(Icons.height),
                             hintText: "Enter height in CM",
@@ -70,23 +86,25 @@ class Bmi extends StatelessWidget {
                           ),
                         ),
                     
-                        Container(
-                          height: 30,
-                          width:double.infinity,
-                          decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                              colors: [Colors.blueAccent, Colors.greenAccent],
-                              begin: AlignmentGeometry.topCenter,
-                              end: AlignmentGeometry.bottomCenter,
+                      GestureDetector(onTap: calculate_bmi,
+                          child: Container(
+                            height: 30,
+                            width:double.infinity,
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [Colors.blueAccent, Colors.greenAccent],
+                                begin: AlignmentGeometry.topCenter,
+                                end: AlignmentGeometry.bottomCenter,
+                              ),
+                            ),
+                            child: Center(
+                              child: Text(
+                                "CALCULATE",
+                                style: TextStyle(color: Colors.white, fontSize: 20),
+                              ),
                             ),
                           ),
-                          child: Center(
-                            child: Text(
-                              "CALCULATE",
-                              style: TextStyle(color: Colors.white, fontSize: 20),
-                            ),
-                          ),
-                        ),
+                        ),Text(result)
                       ],
                     ),
                   ),
